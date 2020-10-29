8f9a34ef6ea8d97fb1d2f02c14db98e43e1f391f
apem <319284373@qq.com>
2020-10-29 10:42:36 +0800

    配置登录路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,21 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Login',
+    component: () => import('@/views/account/Login.vue'),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+    path: '/news',
+    name: 'News',
+    component: () => import('@/views/News.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-});
+  routes,
+})
 
-export default router;
+export default router
---
+++ src/views/account/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-login">
+    登录页
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Login',
+})
+</script>
+
+<style lang="scss" scoped></style>
